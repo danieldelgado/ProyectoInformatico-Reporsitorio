13460bb0b34f5479dea53ac8eed82ba845fd0c23
danieldelgado <danieldelgado20g@gmail.com>
2016-04-21 00:23:49 -0500

    multiportlet

---
+++ SDK/liferay-plugins-sdk/portlets/Reclutamiento-portlet/docroot/WEB-INF/src/com/hitss/rev/controller/ActualizarSolicitudReclutamientoController.java
@@ -0,0 +1,31 @@
+package com.hitss.rev.controller;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import com.hitss.rev.service.ActualizarSolicitudReclutamientoService;
+
+@Controller("actualizarSolicitudReclutamientoController")
+@RequestMapping(value = "VIEW")
+public class ActualizarSolicitudReclutamientoController {
+
+	@Autowired
+	private ActualizarSolicitudReclutamientoService actualizarSolicitudReclutamientoService;
+	
+	@RenderMapping
+	public String defaultView(RenderRequest request, RenderResponse response, Model model) {
+
+		System.out.println(actualizarSolicitudReclutamientoService);
+
+		return "view";
+
+	}
+	
+
+}
---
+++ SDK/liferay-plugins-sdk/portlets/Reclutamiento-portlet/docroot/WEB-INF/src/com/hitss/rev/service/impl/ActualizarSolicitudReclutamientoServiceImp.java
@@ -0,0 +1,10 @@
+package com.hitss.rev.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.hitss.rev.service.ActualizarSolicitudReclutamientoService;
+
+@Service("ActualizarSolicitudReclutamientoService")
+public class ActualizarSolicitudReclutamientoServiceImp implements ActualizarSolicitudReclutamientoService {
+
+}
