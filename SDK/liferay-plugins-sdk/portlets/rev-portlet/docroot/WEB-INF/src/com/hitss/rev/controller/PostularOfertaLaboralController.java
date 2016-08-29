30641e4f478de41787933defd7baaeb93a6e9dcd
crossfire <danieldelgado20g@gmail.com>
2016-08-29 11:35:41 -0500

    Base de datos actualizada y nuevo portlet de postular oferta

---
+++ SDK/liferay-plugins-sdk/portlets/rev-portlet/docroot/WEB-INF/src/com/hitss/rev/controller/PostularOfertaLaboralController.java
@@ -0,0 +1,31 @@
+package com.hitss.rev.controller;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import com.hitss.rev.util.RevController;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+
+
+@Controller("postularOfertaLaboralController")
+@RequestMapping(value = "VIEW")
+public class PostularOfertaLaboralController extends RevController{
+
+
+	private static Log _log = LogFactoryUtil.getLog(PostularOfertaLaboralController.class);
+
+	@RenderMapping
+	public String defaultView(RenderRequest request, RenderResponse response, Model model) {
+		_log.debug("defaultView");
+		return "view";
+	}
+
+
+}
